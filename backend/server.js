f37086a8ebe07fa4e333d8f777010ea8fe6a5a53
1106137105 <1106137105@nkust.edu.tw>
2021-04-20 16:46:45 +0800

    backend ES module setup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
-const express = require('express')
-const products = require('./data/products')
-const dotenv = require('dotenv')
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
+
 dotenv.config()
-
-
 
 const app = express()
 
